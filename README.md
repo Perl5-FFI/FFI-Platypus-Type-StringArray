41f013632cdfa98365cbaeebe6da11a981368675
Graham Ollis <plicease@cpan.org>
2015-01-23 15:11:32 -0500

    initial implementation

---
+++ README.md
@@ -0,0 +1,46 @@
+# FFI::Platypus::Type::StringArray
+
+Platypus custom type for arrays of strings
+
+# SYNOPSIS
+
+In your C code:
+
+    void
+    takes_string_array(const char **array)
+    {
+      ...
+    }
+
+In your [Platypus::FFI](https://metacpan.org/pod/Platypus::FFI) code:
+
+    use FFI::Platypus::Declare
+      'void',
+      [ '::StringArray' => 'string_array' ];
+    
+    attach takes_string_array => [string_array] => void;
+    
+    my @list = qw( foo bar baz );
+    
+    takes_string_array \@list;
+
+# DESCRIPTION
+
+This module provides a [FFI::Platypus](https://metacpan.org/pod/FFI::Platypus) custom type for arrays of strings.
+It is not (yet) supported as a return type.
+
+# SEE ALSO
+
+- [FFI::Platypus](https://metacpan.org/pod/FFI::Platypus)
+- [FFI::Platypus::Type::StringPointer](https://metacpan.org/pod/FFI::Platypus::Type::StringPointer)
+
+# AUTHOR
+
+Graham Ollis <plicease@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2015 by Graham Ollis.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/FFI/Platypus/Type/StringArray.pm
+++ lib/FFI/Platypus/Type/StringArray.pm
@@ -6,4 +6,78 @@ use warnings;
 # ABSTRACT: Platypus custom type for arrays of strings
 # VERSION
 
+=head1 SYNOPSIS
+
+In your C code:
+
+ void
+ takes_string_array(const char **array)
+ {
+   ...
+ }
+
+In your L<Platypus::FFI> code:
+
+ use FFI::Platypus::Declare
+   'void',
+   [ '::StringArray' => 'string_array' ];
+ 
+ attach takes_string_array => [string_array] => void;
+ 
+ my @list = qw( foo bar baz );
+ 
+ takes_string_array \@list;
+
+=head1 DESCRIPTION
+
+This module provides a L<FFI::Platypus> custom type for arrays of strings.
+It is not (yet) supported as a return type.
+
+=cut
+
+use constant _incantation =>
+  $^O eq 'MSWin32' && $Config::Config{archname} =~ /MSWin32-x64/
+  ? 'Q'
+  : 'L!';
+
+my @stack;
+
+sub ffi_custom_type_api_1
+{
+  #my($ffi, $count) = @_;
+  
+  return {
+  
+    native_type => 'opaque',
+    
+    perl_to_native => sub {
+      my $argc = scalar @{ $_[0] };
+      my @c_strings = map { "$_\0" } @{ $_[0] };
+      my $ptrs = pack 'P' x $argc, @c_strings, 0;
+      my $argv = unpack _incantation, pack 'P', $ptrs;
+      push @stack, [ \@c_strings, \$ptrs ];
+      $argv;
+    },
+    
+    perl_to_native_post => sub {
+      pop @stack;
+      ();
+    },
+  
+  };
+}
+
 1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<FFI::Platypus>
+
+=item L<FFI::Platypus::Type::StringPointer>
+
+=back
+
+=cut
